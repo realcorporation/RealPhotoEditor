de90840226386231b2e7265216478e901597195f
kingho <king@real.co>
2018-09-19 18:06:42 +0800

    Added framework support.

---
+++ Photo Editor/REPhotoEditor/REPhotoEditor.h
@@ -0,0 +1,19 @@
+//
+//  REPhotoEditor.h
+//  REPhotoEditor
+//
+//  Created by king on 19/9/2018.
+//  Copyright © 2018 Mohamed Hamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for REPhotoEditor.
+FOUNDATION_EXPORT double REPhotoEditorVersionNumber;
+
+//! Project version string for REPhotoEditor.
+FOUNDATION_EXPORT const unsigned char REPhotoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <REPhotoEditor/PublicHeader.h>
+
+
